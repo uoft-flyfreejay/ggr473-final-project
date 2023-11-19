08118b9579e3b303d781ca394b7b05491983ea8f
uoft-flyfreejay <96443825+uoft-flyfreejay@users.noreply.github.com>
2023-11-19 05:40:07 -0500

    uploading an initial sql file
    sql queries used for joining census tract

---
+++ joining_tract.sql
@@ -0,0 +1,7 @@
+UPDATE cleaned_census
+SET NAME = REPLACE(NAME, ', Cook County, Illinois', '')
+WHERE NAME LIKE '%, Cook County, Illinois';
+
+SELECT *
+FROM boundaries_census
+JOIN cleaned_census on boundaries_census.namelsad10 = cleaned_census.NAME;
