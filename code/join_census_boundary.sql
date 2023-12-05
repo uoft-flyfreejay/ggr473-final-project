ada142d50adfffc43d7710811e649c1b4c6a01e3
MissyZhang <123055808+MissyZhang@users.noreply.github.com>
2023-12-04 20:57:27 -0500

    code cleaning

--- code/join_census_boundary.sql
+++ code/join_census_boundary.sql
@@ -1,45 +1,36 @@
-UPDATE cleaned_census
+UPDATE Excel_cleaned_census
 SET NAME = REPLACE(NAME, ', Cook County, Illinois', '')
 WHERE NAME LIKE '%, Cook County, Illinois';
 
-ALTER TABLE cleaned_census
-ADD COLUMN average_vehicles FLOAT;
+ALTER TABLE Excel_cleaned_census
+ADD COLUMN percent_households_with_car FLOAT;
 
-DELETE FROM cleaned_census
-WHERE medianincome = '-' AND houseunits = '0';
+DELETE FROM Excel_cleaned_census
+WHERE medianincome = '-' ;
 
-UPDATE cleaned_census
-SET medianincome = '0'
-WHERE medianincome = '-';
-
-DELETE FROM cleaned_census
-where id = 1;
-
-UPDATE cleaned_census
+UPDATE Excel_cleaned_census
 SET medianincome = '250000'
 WHERE medianincome = '250,000+';
 
-ALTER TABLE cleaned_census
+ALTER TABLE Excel_cleaned_census
 ALTER COLUMN novehicle TYPE INT USING novehicle::INT,
 ALTER COLUMN onevehicle TYPE INT USING onevehicle::INT,
 ALTER COLUMN twovehicle TYPE INT USING twovehicle::INT,
 ALTER COLUMN threevehicle TYPE INT USING threevehicle::INT,
-ALTER COLUMN medianincome TYPE INT USING medianincome::INT,
-ALTER COLUMN houseunits TYPE INT USING houseunits::INT;
+ALTER COLUMN medianincome TYPE INT USING medianincome::INT;
 
-UPDATE cleaned_census
-SET average_vehicles = (onevehicle + 2 * twovehicle + 3 * threevehicle) * 1.0 / houseunits;
+UPDATE Excel_cleaned_census
+SET percent_households_with_car = ((onevehicle + twovehicle + threevehicle) / (novehicle + onevehicle + twovehicle + threevehicle)) * 100;
 
-CREATE VIEW "boundaries_census_data" AS
+CREATE VIEW "sql_cleaned_data" AS
 SELECT 
 	boundaries_census.*,
-	cleaned_census.name,
-	cleaned_census.novehicle,
-	cleaned_census.onevehicle,
-	cleaned_census.twovehicle,
-	cleaned_census.threevehicle,
-	cleaned_census.medianincome,
-	cleaned_census.houseunits,
-	cleaned_census.average_vehicles
+	Excel_cleaned_census.name,
+	Excel_cleaned_census.novehicle,
+	Excel_cleaned_census.onevehicle,
+	Excel_cleaned_census.twovehicle,
+	Excel_cleaned_census.threevehicle,
+	Excel_cleaned_census.medianincome,
+	Excel_cleaned_census.percent_households_with_car
 FROM boundaries_census
-JOIN cleaned_census on boundaries_census.namelsad10 = cleaned_census.NAME;
+JOIN Excel_cleaned_census on boundaries_census.namelsad10 = Excel_cleaned_census.NAME;
